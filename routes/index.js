fcddba78d8049ad1a25b6869c97acfcd65c3f309
Semi <yomisemite@gmail.com>
2024-03-07 00:30:52 -0500

    add initial routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import quotesRoutes from "./quotes.js"
 
 const router = Router();
 
 router.get("/", (req, res) => res.send("This is the api root!"));
+
+router.use("/quotes", quotesRoutes)
 
 export default router;
