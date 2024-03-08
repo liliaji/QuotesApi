01d0a667e836a626ec0de90ba469d4ecd260a0d6
Semi <yomisemite@gmail.com>
2024-03-08 10:07:13 -0500

    added authorImage

--- models/Quote.js
+++ models/Quote.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema
 
 let QuoteSchema = new Schema({
   "author": { type: String },
+  "authorImage": { type: String},
   "content": { type: String },
   "tag": { type: String },
   "length": { type: Number },
