68811e5327cde24f890893355b2b46eb8bebee48
Semi <yomisemite@gmail.com>
2024-03-13 09:41:04 -0400

    edit connection file

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 import chalk from "chalk";
 
+let connectionString = process.env.DB_URL || "mongodb://127.0.0.1:27017/QuotesApi_api"
+mongoose.connect(connectionString)
+
 mongoose.set("returnOriginal", false);
 
-mongoose.connect("mongodb://127.0.0.1:27017/QuotesApi_api").catch((err) => {
+mongoose.connect(connectionString ``).catch((err) => {
   console.log(`Error connection to MongoDB: ${err.message}`);
 });
 
