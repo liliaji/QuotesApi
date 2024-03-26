d187df8d0afe843aa07112e2b41dbfba48fc0fa9
Semi <yomisemite@gmail.com>
2024-03-26 16:11:58 -0400

    update deploy

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import logger from "morgan";
 import chalk from "chalk";
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 console.log(process.env.PORT)
 
