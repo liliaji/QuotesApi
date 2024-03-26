3a58dc02f47e8ce04831568111a7a2c86d37d416
Semi <yomisemite@gmail.com>
2024-03-26 16:48:22 -0400

    deploy ready change

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import chalk from "chalk";
 
-let connectionString = process.env.DB_URL || "mongodb://127.0.0.1:27017/QuotesApi_api"
+const connectionString = process.env.DB_URL || "mongodb://127.0.0.1:27017/QuotesApi_api"
 
 mongoose.set("returnOriginal", false);
 
